f76f1740245a29ad8635d835f8092bb554bf6b6a
MeijerM1 <max1_meijer@hotmail.com>
2020-09-27 10:39:18 +0200

    blurred backgrounds

--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import { Space, Button } from 'antd';
-// import {LinkOutlined} from '@ant-design/icons'
-// import styles from './header.module.less'
+import React from 'react';
+import { Space, Button } from 'antd';
+import {LinkOutlined} from '@ant-design/icons'
+import styles from './header.module.less'
 
-// export const Header = () => {
+export const Header = () => {
 
 
-//     return (
-//         <div className={styles.container}>
-//             <Space>
-//                 <img className={styles.header_img} src="assets/icon.png" alt="the ring" />
-//                 <Button size="large" className={styles.nav_btn} ghost>Home</Button>
-//                 <Button size="large" className={styles.nav_btn}ghost >Patch notes</Button>
-//                 <Button size="large" className={styles.nav_btn}ghost >Forums<LinkOutlined /></Button>
-//                 <Button size="large" className={styles.nav_btn} ghost >About<LinkOutlined /></Button>
-//             </Space>
-//         </div>
-//     )
-// }
+    return (
+        <div className={styles.container}>
+            <Space>
+                <img className={styles.header_img} src="assets/icon.png" alt="the ring" />
+                <Button size="large" className={styles.nav_btn} ghost>Home</Button>
+                <Button size="large" className={styles.nav_btn}ghost >Patch notes</Button>
+                <Button size="large" className={styles.nav_btn}ghost >Forums<LinkOutlined /></Button>
+                <Button size="large" className={styles.nav_btn} ghost >About<LinkOutlined /></Button>
+            </Space>
+        </div>
+    )
+}
